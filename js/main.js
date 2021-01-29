2e79fee227c85381b832755bb6a5a58d45b8f126
e-r-ik <44287767+e-r-ik@users.noreply.github.com>
2021-01-29 12:49:07 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,20 +5,53 @@ let ctx = canvas.getContext("2d");
 
 let arr2d = new Array(2);
 
-let n = 1;
-
-for (let i = 0; i < 10; i++) {
-  for (let j = 1; j <= 10; j++) {
-    ctx.moveTo(i * 100, (j - 1) * 100);
-    ctx.lineTo((i + 1) * 100, (j - 1) * 100);
-    ctx.moveTo(i * 100, (j - 1) * 100);
-    ctx.lineTo(i * 100, j * 100);
-    ctx.stroke();
-    arr2d[i, j] = n;
-
-    ctx.font = "30px Arial";
-    ctx.fillText(arr2d[i, j], i * 100, j * 100);
-
-    n++;
+class Tile {
+  constructor(xStart, yStart, xEnd, yEnd) {
+    this.xStart = xStart;
+    this.yStart = yStart;
+    this.xEnd = xEnd;
+    this.yEnd = yEnd;
   }
 }
+
+function getMousePos(canvas, event) {
+  let rect = canvas.getBoundClientRect();
+  return {
+    x: event.clientX - rect.left,
+    y: event.clientY - rect.top
+  };
+}
+
+function isInside(pos, rect) {
+	return pos.x > rect.x && pos.x < rect.x+rect.width && pos.y < rect.y+rect.heigth && pos.y > rect.y
+}
+
+canvas.addEventListener("click", function(evt) {
+  let mousePos = getMousePos(canvas, evt);
+    debugger;
+  if (isInside(mousePos,)) {
+
+  }
+})
+
+function Game() {
+  let n = 1;
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      arr2d[i, j] = new Tile(i * 100, j * 100, (i + 1) * 100, (j + 1) * 100);
+      ctx.beginPath();
+      ctx.rect(arr2d[i, j].xStart, arr2d[i, j].yStart, arr2d[i, j].xEnd, arr2d[i, j].yEnd);
+      ctx.stroke();
+
+
+      ctx.font = "30px Arial";
+      ctx.fillText(n, j * 100, (i + 1) * 100);
+
+      n++;
+    }
+  }
+
+}
+
+Game();

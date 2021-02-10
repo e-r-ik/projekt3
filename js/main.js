e017c340ba443b73c2374358e4d0a32476995834
e-r-ik <44287767+e-r-ik@users.noreply.github.com>
2021-02-10 21:39:42 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let arr2d = new Array(2);
-document.addEventListener("click", GetHoverTile(event);
 
 class Tile {
   constructor(xStart, yStart, xEnd, yEnd, number) {
@@ -11,28 +10,16 @@ class Tile {
     this.yStart = yStart;
     this.xEnd = xEnd;
     this.yEnd = yEnd;
+    this.number = number;
   }
 }
 
-function GetHoverTile(event {
-  for (var i = 0; i < arr2d.length; i++) {
-    for (var j = 0; j < arr2d.length; j++) {
-      let currentMouseX = event.clientX;
-      let currentMouseY = event.clientY;
-      if (arr2d[i, j].xStart <= currentMouseX && arr2d[i, j].xEnd >= currentMouseX &&
-          arr2d[i, j].yStart <= currentMouseY && arr2d[i, j].yEnd >= currentMouseY) {
-        alert("du tryckte på tile" + arr[i, j].number);
-      }
-    }
-  }
-}
-
-function StartGame() {
+function startGame() {
   let n = 1;
 
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
-      arr2d[i, j] = new Tile(i * 100, j * 100, (i + 1) * 100, (j + 1) * 100);
+      arr2d[i, j] = new Tile(i * 100, j * 100, (i + 1) * 100, (j + 1) * 100, n);
       ctx.beginPath();
       ctx.rect(arr2d[i, j].xStart, arr2d[i, j].yStart, arr2d[i, j].xEnd, arr2d[i, j].yEnd, n);
       ctx.stroke();
@@ -46,8 +33,31 @@ function StartGame() {
   }
 }
 
-StartGame();
-
-function Looper() {
-
+function getMousePos(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top
+    };
 }
+
+function isInside(pos, rect){
+    return pos.x > rect.x && pos.x < rect.x+rect.width && pos.y < rect.y+rect.height && pos.y > rect.y
+}
+
+canvas.addEventListener('click', function(evt) {
+    let mousePos = getMousePos(canvas, evt);
+
+    for (var i = 0; i < arr2d.length; i++) {
+      for (var j = 0; j < arr2d.length; j++) {
+        if (isInside(mousePos, arr2d[i, j])) {
+            console.log(arr2d[i, j].number);
+        }else {
+            console.log("nope");
+        }
+      }
+    }
+
+}, false);
+
+startGame();

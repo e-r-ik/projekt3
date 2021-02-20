d34a5c02bfa4c582219cc0ef61669dd6a417a40f
e-r-ik <44287767+e-r-ik@users.noreply.github.com>
2021-02-20 21:30:41 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
-let _gameCanvas = document.getElementById("game");
-let _arr2d = [
+let gameCanvas = document.getElementById("game");
+let arr2d = [
   [],
   [],
   [],
@@ -15,28 +15,30 @@ let _arr2d = [
 ];
 
 class Tile {
-  constructor(element, style, onclick) {
+  constructor(element, style, onclick, isBoat) {
     this.element = element;
     this.style = style;
     this.onclick = onclick;
+    this.isboat = isBoat;
   }
 }
 
 function drawGame() {
-  for (let i = 0; i < _arr2d.length; i++) {
-    for (let j = 0; j < _arr2d.length; j++) {
+  for (let i = 0; i < arr2d.length; i++) {
+    for (let j = 0; j < arr2d.length; j++) {
 
-      _arr2d[j][i] = new Tile(document.createElement("div"),
-                              document.createAttribute("class"),
-                              document.createAttribute("onclick"));
+      arr2d[j][i] = new Tile(document.createElement("div"),
+                             document.createAttribute("class"),
+                             document.createAttribute("onclick"),
+                             false);
 
-      _arr2d[j][i].style.value = "tileStyle";
-      _arr2d[j][i].onclick.value = "console.log('du tryckte')";
+      arr2d[j][i].style.value = "tileStyle";
+      arr2d[j][i].onclick.value = "console.log('du tryckte')";
 
-      _gameCanvas.appendChild(_arr2d[j][i].element);
+      gameCanvas.appendChild(arr2d[j][i].element);
 
-      _arr2d[j][i].element.setAttributeNode(_arr2d[j][i].style);
-      _arr2d[j][i].element.setAttributeNode(_arr2d[j][i].onclick);
+      arr2d[j][i].element.setAttributeNode(arr2d[j][i].style);
+      arr2d[j][i].element.setAttributeNode(arr2d[j][i].onclick);
     }
   }
 }
